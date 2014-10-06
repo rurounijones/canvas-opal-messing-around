11e7d6226a3afe4791ae7b25f781a3b53258d679
Jeffrey Jones <jeff@jones.be>
2014-10-06 09:45:26 +0900

    Add zooming

--- app/map.rb
+++ app/map.rb
@@ -3,18 +3,19 @@ require 'opal-jquery'
 require 'systems'
 
 class Map
-  attr_reader :height, :width, :canvas, :context, :x, :y, :dragging
+  attr_accessor :height, :width, :canvas, :context, :x, :y, :dragging, :zoom
 
   def initialize
-    @width   = `800`
-    @height  = `500`
+    @height  = `$(window).height() * 0.9`
+    @width   = `$(window).width() * 0.9`
     @canvas  = `document.getElementById(#{canvas_id})`
     @context = `#{canvas}.getContext('2d')`
     @x = @width / 2
     @y = (@height / 2) * -1.0
-    @draging = false
+    @dragging = false
     @drag_start_x = 0
     @drag_start_y = 0
+    @zoom = 1
     Element.find("##{canvas_id}").on :mousedown do |event|
       @drag_start_x = event[:clientX] - `#{rect}.left`
       @drag_start_y = event[:clientY] - `#{rect}.top`
@@ -58,6 +59,19 @@ class Map
       @dragging = false
     end
 
+    Element.find("##{canvas_id}").on :mousewheel do |event|
+      @mag = event[:deltaY]
+      if @mag > 0
+        @zoom = @zoom * 2
+        @x = @x / 2
+        @y= @y / 2
+      elsif @zoom >= 1
+        @zoom = @zoom / 2
+        @x = @x * 2
+        @y = @y * 2
+      end
+      draw_planets
+    end
   end
 
   def rect
@@ -85,13 +99,13 @@ class Map
   end
 
   def draw_planet(name, system_x, system_y, faction)
-    system_x = (system_x + x)
-    system_y = (system_y + y) * -1.0
+    system_x = (system_x + x) * zoom
+    system_y = ((system_y + y) * -1.0) * zoom
 
-    if system_x > 0 && system_x < @width && system_y > 0 && system_y < @height #Only if the systems are in the viewport
-      if ["Tharkad", "Terra", "New Avalon", "Luthien", "Atreus (FWL)", "Sian", "Strana Mechty"].include?(name)
+    if system_x > 0 && system_x < self.width && system_y > 0 && system_y < self.height #Only if the systems are in the viewport
+      if ["Tharkad", "Terra", "New Avalon", "Luthien", "Atreus (FWL)", "Sian", "Strana Mechty"].include?(name) || @zoom > 3
         `#{context}.fillStyle = "#fff"`
-        `#{context}.fillText(#{name}, #{system_x.floor+5}, #{system_y.floor+6})`
+        `#{context}.fillText(#{name}, #{system_x.floor + 5 + @zoom}, #{system_y.floor + 6 + @zoom})`
       end
 
       if faction[0..1] == "DC"
@@ -107,7 +121,7 @@ class Map
       else
         `#{context}.fillStyle = "#fff"`
       end
-      `#{context}.fillRect(#{system_x.floor+1}, #{system_y.floor+1}, 1, 1)`
+      `#{context}.fillRect(#{system_x.floor + 1}, #{system_y.floor + 1}, #{self.zoom}, #{self.zoom})`
     end
   end
 
